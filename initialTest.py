330a96b845f8f172001604631ecfa2ed5988f684
pwilliams1 <P.Williams.2@warwick.ac.uk>
2016-09-01 13:50:21 +0100

    Start editing test for stats page

--- initialTest.py
+++ initialTest.py
@@ -6,3 +6,12 @@ class statsPageTests(unittest.TestCase):
 	def test_softwareName(self):
 	
 		
+		
+if name == "__main__":
+
+	if len(sys.argv) > 1:
+	
+		statsPageTests.nidmFile = sys.argv.pop()
+		statsPageTests.outputFolder = sys.argv.pop()
+		statsPageTests.softwareName = sys.argv.pop()
+		statsPageTests.softwareVersionNum = sys.argv.pop()
