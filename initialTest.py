bcf79a40d6224d2d93678d5e9d196e12d774ae8f
pwilliams1 <P.Williams.2@warwick.ac.uk>
2016-09-02 08:26:11 +0100

    Test for FSL string

--- initialTest.py
+++ initialTest.py
@@ -1,18 +1,31 @@
 import os
 import unittest
+import sys
 
-class statsPageTests(unittest.TestCase):
-
+class fslTests(unittest.TestCase):
+	htmlFile = ""
 	def test_softwareName(self):
 	
-		x = open(testFileName, "r")
+		file = open(self.htmlFile, "r")
 		found = False
-		for line in x:
-			if "SPM" in line:
+		for line in file:
+			if "FSL" in line:
 				found = True
 				
 		
 		self.assertTrue(found)
+		
+	def test_versionNum(self):
+	
+		#x = open(testFileName, "r")
+		y = 5
 	
 		
 		
+if __name__ == "__main__":
+
+	if len(sys.argv) > 1:
+	
+		fslTests.htmlFile = sys.argv.pop()
+		
+	unittest.main()
