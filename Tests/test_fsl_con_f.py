ce10fdeb119da0cba30c5f60860574002fdb2f7c
pwilliams1 <P.Williams.2@warwick.ac.uk>
2016-09-07 15:56:00 +0100

    Add test_fsl_con_f.py to test viewer.py on nidm file

---
+++ Tests/test_fsl_con_f.py
@@ -0,0 +1,33 @@
+import unittest
+import viewer
+import sys
+import os
+
+class fsl_con_f(unittest.TestCase):
+
+	def setUp(self): #Open necessary file
+	
+		self.data = "fsl_con_f.nidm.ttl"
+		#self.file = open(self.data, "r")
+		self.folder = viewer.main(self.data, "fsl_con_f_testHTML")
+		self.direc = os.getcwd()
+		self.dest = os.path.join(self.direc, self.folder)
+		self.stats = os.path.join(self.dest, "stats.html")
+		self.file = open(self.stats, "r")
+		
+
+	def test_softwareName(self): #Test to see if FSL is in html file
+	
+		for line in self.file:
+		
+			if "FSL" in line:
+			
+				myString = line
+				break
+				
+		self.assertIn("FSL", myString)
+	
+	
+	def tearDown(self):
+	
+		self.file.close()
