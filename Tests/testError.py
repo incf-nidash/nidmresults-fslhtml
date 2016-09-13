eabd51689e54d5a8a7095e2a157d22ee10f7e7f5
pwilliams1 <P.Williams.2@warwick.ac.uk>
2016-09-13 13:39:33 +0100

    Add comments

--- Tests/testError.py
+++ Tests/testError.py
@@ -8,16 +8,16 @@ import glob
 
 class generalTests(unittest.TestCase):
 
-	def test_error(self):
+	def test_error(self): #Run viewer.py on all turtle files in data folder - Checks for errors (program crashes)
+	
 		script = os.path.dirname(os.path.abspath(__file__))
 		dataFolder = os.path.join(script, "data")
 		globData = os.path.join(dataFolder,"*.ttl")
 		data = glob.glob(globData)
 		
+		for i in data: #Loop over all turtle files in data
 		
-		for i in data:
-		
-			viewer.main(i,i + "test")
+			viewer.main(i,i + "test") #Run viewer on turtle file
 			
 if __name__ == "__main__":
 
--- Tests/test_fslViewer.py
+++ Tests/test_fslViewer.py
@@ -323,11 +323,13 @@ class spm_thr_voxelunct4(unittest.TestCase):
 		self.postStatsFile.close()
 		
 if __name__ == "__main__":
-	scriptPath = os.path.dirname(os.path.abspath(__file__))
+	scriptPath = os.path.dirname(os.path.abspath(__file__)) #Get path of script
 	dataPath = os.path.join(scriptPath, "data")
 	globData = os.path.join(dataPath,"*.ttl")
 	data = glob.glob(globData)
-	for i in data:
+	
+	for i in data: #Loop over all turtle files in data folder and create html
 	
 		viewer.main(i, i + "TestResults")
-	unittest.main()
+		
+	unittest.main() #Tests
