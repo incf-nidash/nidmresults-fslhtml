ade48a99e929a7ee49fd24d20c4d253b6a32e2a9
pwilliams1 <P.Williams.2@warwick.ac.uk>
2016-09-16 09:40:09 +0100

    Begin editing testError.py to check if data is on local machine

--- Tests/testError.py
+++ Tests/testError.py
@@ -20,7 +20,19 @@ class generalTests(unittest.TestCase):
 			viewer.main(i,i + "test") #Run viewer on turtle file
 			
 if __name__ == "__main__":
-
+	
+	scriptDir = os.path.dirname(os.path.abspath(__file__))
+	dataDir = os.path.join(scriptDir, "data")
+	dataNames = ["fsl_con_f", "fsl_thr_clustfwep05","ex_spm_thr_voxelunct4","ex_spm_thr_clustunck10","ex_spm_thr_voxelfdrp05"]
+	
+	for dataName in dataNames: #Checks if data is on local machine
+	
+		local = True
+		if os.path.isfile(os.path.join(dataName, ".nidm.ttl")) == False: #Data not found on local machine
+		
+			local = False
+			break
+	
 	unittest.main()
 	
 		
