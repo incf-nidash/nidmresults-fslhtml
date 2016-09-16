722d1d5c3f44c5f54fab42bc4458f4d627094c7b
pwilliams1 <P.Williams.2@warwick.ac.uk>
2016-09-16 10:26:13 +0100

    Begin writing code to get data from neurovault api

--- Tests/testError.py
+++ Tests/testError.py
@@ -5,6 +5,8 @@ import sys
 import string
 import viewer
 import glob
+import urllib.request
+import json
 
 class generalTests(unittest.TestCase):
 
@@ -23,16 +25,26 @@ if __name__ == "__main__":
 	
 	scriptDir = os.path.dirname(os.path.abspath(__file__))
 	dataDir = os.path.join(scriptDir, "data")
-	dataNames = ["fsl_con_f", "fsl_thr_clustfwep05","ex_spm_thr_voxelunct4","ex_spm_thr_clustunck10","ex_spm_thr_voxelfdrp05"]
-	
+	dataNames = ["fsl_con_", "fsl_thr_clustfwep05","ex_spm_thr_voxelunct4","ex_spm_thr_clustunck10","ex_spm_thr_voxelfdrp05"]
+	local = True
 	for dataName in dataNames: #Checks if data is on local machine
 	
-		local = True
-		if os.path.isfile(os.path.join(dataName, ".nidm.ttl")) == False: #Data not found on local machine
 		
+		if os.path.isfile(os.path.join(dataDir, dataName + ".nidm.ttl")) == False: #Data not found on local machine
+			print(dataName)
 			local = False
 			break
 	
-	unittest.main()
+	if local == False:
+		print("Downloading data")
+		req = urllib.request.Request("http://neurovault.org/api/collections/1692/images")
+		resp = urllib.request.urlopen(req)
+		readResp = resp.read()
+		data = json.loads(readResp.decode('utf-8'))
+		print(data)
+		
+		
+	
+	#unittest.main()
 	
 		
