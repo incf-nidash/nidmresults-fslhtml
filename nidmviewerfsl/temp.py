0a17ec5d78c43b55991d875b06c224580bd45751
Tom Maullin <tommaullin@gmail.com>
2017-11-29 16:53:06 +0000

    excursion set problem

--- nidmviewerfsl/temp.py
+++ nidmviewerfsl/temp.py
@@ -33,8 +33,8 @@ def printQuery(query): #Generic function for printing the results of a query - u
 	#ENDFOR
 
 g = rdflib.Graph()
-#turtleFile = glob.glob('/home/tom/Documents/Repos/nidmresults-fslhtml/Tests/data/ex_spm_conjunction_test/nidm.ttl')
-turtleFile = glob.glob('/home/tom/Documents/Repos/nidmresults-fslhtml/Tests/data/ex_spm_contrast_mask_test/nidm.ttl')
+turtleFile = glob.glob('/home/tom/Documents/Repos/nidmresults-fslhtml/Tests/data/ex_spm_conjunction_test/nidm.ttl')
+#turtleFile = glob.glob('/home/tom/Documents/Repos/nidmresults-fslhtml/Tests/data/ex_spm_contrast_mask_test/nidm.ttl')
 
 print(turtleFile)
 g.parse(turtleFile[0], format = "turtle")
@@ -43,9 +43,20 @@ query = """prefix nidm_Inference: <http://purl.org/nidash/nidm#NIDM_0000049>
 			   prefix nidm_ExcursionSetMap: <http://purl.org/nidash/nidm#NIDM_0000025>
                prefix nidm_contrastName: <http://purl.org/nidash/nidm#NIDM_0000085>
                prefix prov: <http://www.w3.org/ns/prov#>
+               prefix nidm_ConjunctionInference: <http://purl.org/nidash/nidm#NIDM_0000011>
+               prefix spm_PartialConjunctionInference: <http://purl.org/nidash/spm#SPM_0000005>
 			   prefix dc: <http://purl.org/dc/elements/1.1/>
 
-               SELECT ?image WHERE {?image a nidm_Inference:}"""
-			
+               SELECT ?image WHERE {{?x a nidm_Inference:} UNION {?x a nidm_ConjunctionInference:} UNION {?x a spm_PartialConjunctionInference:}. ?y prov:wasGeneratedBy ?x . ?y a nidm_ExcursionSetMap: . ?y prov:atLocation ?image}"""
+
+query = """prefix nidm_Inference: <http://purl.org/nidash/nidm#NIDM_0000049>
+               prefix nidm_StatisticMap: <http://purl.org/nidash/nidm#NIDM_0000076>
+               prefix nidm_contrastName: <http://purl.org/nidash/nidm#NIDM_0000085>
+               prefix prov: <http://www.w3.org/ns/prov#>
+               prefix nidm_ConjunctionInference: <http://purl.org/nidash/nidm#NIDM_0000011>
+               prefix spm_PartialConjunctionInference: <http://purl.org/nidash/spm#SPM_0000005>
+
+               SELECT ?contrastName WHERE {{?x a nidm_Inference:} UNION {?x a nidm_ConjunctionInference:} UNION {?x a spm_PartialConjunctionInference:}. ?x prov:used ?y .?z prov:wasGeneratedBy ?x . ?z a nidm_ExcursionSetMap: . ?y a nidm_StatisticMap: . ?y nidm_contrastName: ?contrastName .}"""
+		
 queryResult = g.query(query)
 printQuery(queryResult)
