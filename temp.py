b5736bbdfbb3850fefc23c8e5cce9def0b20adbb
Tom Maullin <tommaullin@gmail.com>
2017-12-01 14:02:30 +0000

    cleanup

--- temp.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import os
-subprocess.call(['easy_install', 'Pillow'])
-subprocess.call(['python', 'setup.py', 'install'])
-curdir = os.getcwd()
-os.chdir('C:\\Users\\owner\\Downloads')
-subprocess.call(['pip', 'install', 'Pillow-3.4.2-cp36-cp36m-win32.whl'])
-os.chdir(curdir)
