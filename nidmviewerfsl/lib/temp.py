a39b129f45a6f2ad8efe3ac4bda5642b3d289ef2
Tom Maullin <tommaullin@gmail.com>
2018-01-19 17:06:08 +0000

    wip query written

---
+++ nidmviewerfsl/lib/temp.py
@@ -0,0 +1,18 @@
+import os
+import glob
+import rdflib
+from queries.queryTools import runQuery, printQuery
+from scipy.misc import toimage
+
+turtleFile = glob.glob('/home/tom/Documents/Repos/nidmresults-fslhtml/nidmviewerfsl/tests/data/ex_spm_conjunction_test/nidm.ttl')
+#turtleFile = glob.glob('C:/Users/owner/Documents/NISOx/Peters_Viewer/nidmresults-fslhtml/Tests/data/ex_spm_partial_conjunction_test/nidm.ttl')
+
+print(turtleFile)
+g = rdflib.Graph()
+g.parse(turtleFile[0], format = "turtle")
+
+t = runQuery(g, 'selectContrastVector', 'Select')
+print(runQuery(g, 'selectContrastVector', 'Select'))
+
+data = float(t[2])
+toimage(data).show()
